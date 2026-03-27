f4640418af7902e67bb1638f507cc1956d0cd885
jcastro <jcastro@cari.net>
2026-03-27 16:48:10 -0600

    Enhance checkout error handling and response validation in ExternalServices

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -5,7 +5,6 @@ loadHeaderFooter();
 
 const order = new CheckoutProcess("so-cart", ".checkout-summary");
 order.init();
-
 // Add event listeners to fire calculateOrderTotal when the user changes the zip code
 document
     .querySelector("#zip")
